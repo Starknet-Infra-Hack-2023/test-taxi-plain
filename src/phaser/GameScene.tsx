f1062921263060042d8423b81391dec0278ce1cb
0xhatsume <0xhatsume@gmail.com>
2023-12-08 20:11:20 +0800

    :coffin: Remove dead comments

--- src/phaser/GameScene.tsx
+++ src/phaser/GameScene.tsx
@@ -20,9 +20,8 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('roads', 'tiles/taxi_background.png');
         this.load.tilemapTiledJSON('roadtilemap', 'maps/GreyGround.json');
-        //this.load.atlas('taxi1', 'characters/taxi.png', 'characters/taxi.json')
-        // this.load.atlas('customer1', 'characters/taxi.png', 'characters/taxi.json')
-        // this.load.atlas('hotel1', 'characters/taxi.png', 'characters/taxi.json')
+        this.load.atlas('customer1', 'characters/taxi.png', 'characters/taxi.json')
+        this.load.atlas('hotel1', 'characters/taxi.png', 'characters/taxi.json')
 
         this.load.spritesheet("taxi1", "characters/taxispritesheet.png", {
             frameWidth: 40,
@@ -39,16 +38,8 @@ class GameScene extends Phaser.Scene {
         this.ground = map.createLayer('ground', "taxi_background", 0, 0);
         this.ground.scale = this.scalefactor
 
-        //createAnims(this.anims)
-
         this.player1 = this.add.sprite(80, 80, "taxi1");
-        //this.player1.setFrame("cab_front.png");
-        //this.player1 = new Taxi(this, 100, 100, 'taxi1', 'cab_front.png')
         this.player1.scale = this.scalefactor * 0.8; // 0.8 is the scale of the spritesheet
-        
-        // this.add.existing(this.player1)
-        // this.physics.add.existing(this.player1)
-        
 
         const gridEngineConfig = {
             characters: [
@@ -77,45 +68,8 @@ class GameScene extends Phaser.Scene {
 
         });
 
-        // this.gridEngine.movementStarted().subscribe(({ direction }) => {
-        //     this.player1.anims.play(direction);
-        // });
-        
-
-        // //might not need this as our sprites have not much animation on movement
-        // this.gridEngine.movementStopped().subscribe(({ direction }) => {
-        //     this.player1.anims.stop();
-        //     this.player1.setFrame("cab_front.png");
-        // });
-
-        // this.gridEngine.directionChanged().subscribe(({ direction }) => {
-        //     this.player1.setFrame("cab_front.png");
-        // });
-
-        // createAnims(this.anims)
-        
-        // this.desintation = new Destination(this, 
-        //     ((this.tile_width*5)-this.tile_width/2)*this.scalefactor, 
-        //     50, 'hotel1', 'hotel.png')
-        // this.passenger = new Passenger(this, 50, 50, 'customer1', 'passenger.png')
-        // this.player1 = new Taxi(this, 100, 100, 'taxi1', 'cab_front.png')
-        
-        // this.add.existing(this.desintation)
-        // this.add.existing(this.passenger)
-        // this.add.existing(this.player1)
-        // this.physics.add.existing(this.player1)
-        // this.player1.scale = this.scalefactor
-        // this.passenger.scale = this.scalefactor
-        // this.desintation.scale = this.scalefactor
     }
 
-    // update(t: number, dt: number)
-	// {
-	// 	if (this.player1)
-	// 	{
-	// 		this.player1.update(this.cursors)
-	// 	}
-	// }
     update(t: number, dt: number) {
         if (this.cursors.left.isDown) {
             this.gridEngine.move("taxi1", "left");
