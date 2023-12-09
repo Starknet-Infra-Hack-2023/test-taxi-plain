be396f520f273d2071e3bdccbbb246c79235c8f1
0xhatsume <0xhatsume@gmail.com>
2023-12-09 14:32:40 +0800

    :sparkles: Finalize AI code for front end with q table json

--- src/phaser/GameScene.tsx
+++ src/phaser/GameScene.tsx
@@ -3,6 +3,7 @@ import Taxi from './characters/Taxi';
 import Passenger from './characters/Passenger';
 import Destination from './characters/Destination';
 import { createAnims } from './animations/CreateAnims';
+import q_json from './../ml/q.json';
 
 class GameScene extends Phaser.Scene {
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
@@ -10,6 +11,7 @@ class GameScene extends Phaser.Scene {
     player1!: Phaser.GameObjects.Sprite
     passenger!: Phaser.GameObjects.Sprite
     passengerInTaxi: boolean = false
+    passengerDelivered: boolean = false
     // destinationLocation: number = 0
     // locationMapping = {
     //     0: {x:0, y:0},
@@ -17,10 +19,22 @@ class GameScene extends Phaser.Scene {
     //     2: {x:0, y:4},
     //     3: {x:4, y:4},
     // }
+
+    actionMap = {
+        0: "down",
+        1: "up",
+        2: "right",
+        3: "left",
+        4: "pickup",
+        5: "dropoff"
+    }
+
     desintation!: Phaser.GameObjects.Sprite
     tile_width = 32
     tile_height = 32
     scalefactor = 3
+    ai_pause = true
+    timer = 0;
 
     constructor() {
         super('GameScene');
@@ -38,7 +52,6 @@ class GameScene extends Phaser.Scene {
         this.cursors = this?.input.keyboard.createCursorKeys()
     }
     create() {
-        
 
         const map = this.make.tilemap({ key: 'roadtilemap' });
         const tileset = map.addTilesetImage('taixbgbig', 'roads');
@@ -51,10 +64,7 @@ class GameScene extends Phaser.Scene {
         level3.scale = this.scalefactor;
 
 
-        this.desintation = this.add.sprite(80, 80, "hotel1");
-        this.desintation.setFrame("hotel.png")
-        this.desintation.scale = this.scalefactor;
-        this.desintation.depth = 10;
+        
 
         this.passenger = this.add.sprite(80, 80, "customer1");
         this.passenger.setFrame("passenger.png")
@@ -64,6 +74,11 @@ class GameScene extends Phaser.Scene {
         this.player1 = this.add.sprite(80, 80, "taxi1");
         this.player1.scale = this.scalefactor * 0.8; // 0.8 is the scale of the spritesheet
         this.player1.depth = 30;
+
+        this.desintation = this.add.sprite(80, 80, "hotel1");
+        this.desintation.setFrame("hotel.png")
+        this.desintation.scale = this.scalefactor;
+        this.desintation.depth = 50;
 
         const gridEngineConfig = {
             characters: [
@@ -77,47 +92,32 @@ class GameScene extends Phaser.Scene {
                     }
                 },
                 {
-                    id: "hotel1",
-                    sprite: this.desintation,
-                    startPosition: { x:3, y:4},
-                    charLayer: 'ground',
-                    collides: {
-                        collisionGroups: []
-                    }
-                },
-                
-                {
                     id: "taxi1",
                     sprite: this.player1,
                     walkingAnimationMapping: 0,
-                    startPosition: { x: 3, y: 3 },
+                    startPosition: { x: 2, y: 2 },
                     //charLayer: 'ground',
                     collides: {
                         collisionGroups: []
                     }
                 },
-                
+                {
+                    id: "hotel1",
+                    sprite: this.desintation,
+                    startPosition: { x:3, y:4},
+                    //charLayer: 'ground',
+                    collides: {
+                        collisionGroups: []
+                    }
+                },
             ],
         };
 
         this.gridEngine.create(map, gridEngineConfig);
 
         this.gridEngine.movementStopped().subscribe(() => {
-            const taxiPos = this.player1.getCenter();
-            const tileXY = this.ground.getTileAtWorldXY(taxiPos.x, taxiPos.y);
-            //console.log("taxi position: ",tileXY.x, ",", tileXY.y);
-
-            const passengerPos = this.passenger.getCenter();
-            const tileXY2 = this.ground.getTileAtWorldXY(passengerPos.x, passengerPos.y);
-            //console.log("passenger position: ",tileXY2.x, ",", tileXY2.y);
-            //console.log("passenger code: ", this.getPassengerCode(tileXY2));
-
-            const destinationPos = this.desintation.getCenter();
-            const tileXY3 = this.ground.getTileAtWorldXY(destinationPos.x, destinationPos.y);
-            //console.log("destination position: ",tileXY3.x, ",", tileXY3.y);
-            //console.log("destination code: ", this.getDestinationCode(tileXY3));
-
-            console.log("configcode: ", tileXY.y, tileXY.x, this.getPassengerCode(tileXY2), this.getDestinationCode(tileXY3));
+            const config = this.getCurrentQConfig();
+            this.getQState({...config});
         });
 
         //this.input.on('pointerdown', () => {
@@ -134,8 +134,92 @@ class GameScene extends Phaser.Scene {
             this.dropOffPassenger();
         });
 
+        
+
+        this.input.keyboard.on('keydown-R', () => {
+            console.log("R pressed");
+            this.resetGame();
+        });
+
+        this.getQState({...this.getCurrentQConfig()});
+
+        
+        this.input.keyboard.on('keydown-P', () => {
+            console.log("P pressed");
+            this.pauseUnpauseAI();
+
+        });
     }
 
+    ai_move(){
+        if(!this.ai_pause){
+            console.log("AI is moving");
+
+            // get current q state
+            const config = this.getCurrentQConfig();
+            const state = this.getQState({...config});
+
+            // get action from q_json
+            //console.log("q: ", q_json[state]);
+            const action_key = Object.keys(q_json[state]).reduce((a, b) => q_json[state][a] > q_json[state][b] ? a : b);
+            const action_string = this.actionMap[action_key];
+            console.log("decision is: ", action_string);
+
+            // execute action
+            if(action_string == "pickup") {
+                this.pickupPassenger();
+            } else
+            if(action_string == "dropoff") {
+                this.dropOffPassenger();
+            } else {
+                this.gridEngine.move("taxi1", action_string);
+            }
+        }
+    }
+
+    resetGame(){
+        const pickUpArray = [
+            {x: 0, y: 0},
+            {x: 4, y: 0},
+            {x: 0, y: 4},
+            {x: 3, y: 4},
+        ];
+
+        let random = Math.floor(Math.random() * pickUpArray.length);
+        const passengerPos = pickUpArray[random];
+        pickUpArray.splice(random, 1);
+        random = Math.floor(Math.random() * pickUpArray.length);
+        const hotelPost = pickUpArray[random];
+
+        this.passengerInTaxi = false;
+        this.passengerDelivered = false;
+        
+
+        //this.desintation.setVisible(false);
+        //this.passenger.setVisible(false);
+        this.gridEngine.moveTo("customer1", passengerPos);
+        this.gridEngine.moveTo("hotel1", hotelPost);
+        this.desintation.setVisible(true);
+        this.passenger.setVisible(true);
+
+        this.getQState({...this.getCurrentQConfig()});
+    }
+
+    pauseUnpauseAI() {
+        this.ai_pause = !this.ai_pause;
+        console.log("AI is paused: ", this.ai_pause);
+        // if(this.pause) {
+        //     this.scene.pause();
+        // }else {
+        //     this.scene.resume();
+        // }
+    }
+    getQState({taxiRow, taxiCol, passengerCode, destinationCode}) {
+        // total states: 5 * 5 * 5 * 4 = 500
+        const q_state = (taxiRow*100) + (taxiCol*20) + (passengerCode*4) + destinationCode;
+        console.log("q_state: ", q_state);
+        return q_state;
+    }
     getPassengerCode(tileXY: Phaser.Tilemaps.Tile) {
         if(this.passengerInTaxi){
             return 4;
@@ -162,6 +246,29 @@ class GameScene extends Phaser.Scene {
         }else {
             return 0;
         }
+    }
+    getCurrentQConfig(){
+        const taxiPos = this.player1.getCenter();
+        const tileXY = this.ground.getTileAtWorldXY(taxiPos.x, taxiPos.y);
+        //console.log("taxi position: ",tileXY.x, ",", tileXY.y);
+
+        const passengerPos = this.passenger.getCenter();
+        const tileXY2 = this.ground.getTileAtWorldXY(passengerPos.x, passengerPos.y);
+        //console.log("passenger position: ",tileXY2.x, ",", tileXY2.y);
+        //console.log("passenger code: ", this.getPassengerCode(tileXY2));
+
+        const destinationPos = this.desintation.getCenter();
+        const tileXY3 = this.ground.getTileAtWorldXY(destinationPos.x, destinationPos.y);
+        //console.log("destination position: ",tileXY3.x, ",", tileXY3.y);
+        //console.log("destination code: ", this.getDestinationCode(tileXY3));
+        const config = {
+            taxiRow: tileXY.y, 
+            taxiCol: tileXY.x, 
+            passengerCode: this.getPassengerCode(tileXY2), 
+            destinationCode: this.getDestinationCode(tileXY3)
+        }
+        console.log("configcode: ", config);
+        return config;
     }
 
     pickupPassenger() {
@@ -194,7 +301,7 @@ class GameScene extends Phaser.Scene {
 
             // here resets game
             console.log("game resetting...")
-
+            this.resetGame();
             return true;
         } else if (!this.passengerInTaxi) {
             console.log("No passenger in taxi!");
@@ -205,7 +312,6 @@ class GameScene extends Phaser.Scene {
         }
     }
 
-
     update(t: number, dt: number) {
         if (this.cursors.left.isDown) {
             this.gridEngine.move("taxi1", "left");
@@ -215,6 +321,13 @@ class GameScene extends Phaser.Scene {
             this.gridEngine.move("taxi1", "up");
         } else if (this.cursors.down.isDown) {
             this.gridEngine.move("taxi1", "down");
+        }
+
+        this.timer += dt;
+        if(this.timer > 2000){ // execute every 2 seconds
+            //console.log(t);
+            this.timer = 0;
+            this.ai_move();
         }
     }
 }
